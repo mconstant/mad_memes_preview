8530a4b57e3f40cfbffd720a4a9d1b564e54f16b
mconstant <85532172+mconstant@users.noreply.github.com>
2023-09-13 18:45:18 +0000

    rename to pipeline

---
+++ get_tools.sh
@@ -0,0 +1,17 @@
+# Bash 'Strict Mode'
+# http://redsymbol.net/articles/unofficial-bash-strict-mode
+# https://github.com/xwmx/bash-boilerplate#bash-strict-mode
+set -o nounset
+set -o errexit
+set -o pipefail
+IFS=$'\n\t'
+
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+achmod +x secretcli-Linux
+sudo mv secretcli-Linux /usr/local/bin/secretcli
+. ~/.nix-profile/etc/profile.d/nix.sh
+nix-shell
+rustup default stable
+rustup target add wasm32-unknown-unknown
+rustup install nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly
