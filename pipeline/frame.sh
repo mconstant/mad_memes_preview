f815ad0a3b7827847634223a8cf90b5f18fc65f6
mconstant <85532172+mconstant@users.noreply.github.com>
2023-09-14 21:30:41 +0000

    fix badges

--- pipeline/frame.sh
+++ pipeline/frame.sh
@@ -1,11 +1,25 @@
-
-
+# get width
+echo $7
+width=$(identify -format "%w" $1)
+convert mad_memes_top_logo.png -resize $width tmp_logo.png
+height=$(identify -format "%h" tmp_logo.png)
+convert badge_transparent.png -font Creepster -fill White -pointsize 52 \
+        -annotate +60+150 '#'${8}'' -pointsize 35 -annotate +40+100 'Season 1' tmp_badge.png
+convert tmp_badge.png -resize $height tmp_badge.png
+convert tmp_logo.png tmp_badge.png -gravity East   \
+        -alpha On \
+        -composite  tmp_logo.png
+convert tmp_logo.png $1 -append tmp_output.png
+convert ${7}.png -resize ${width} tmp_rarity.png
+convert tmp_output.png tmp_rarity.png -append tmp_output.png
+convert mad_memes_footer.png -resize $width tmp_footer.png
+convert tmp_output.png tmp_footer.png -append tmp_output.png
 # e.g. ./frame.sh meme.jpg DejaVu-Sans 24 "Mad Meme #666: Legendary" frame.png output.png
 
 
-magick $1 -resize 550x550\> -background Lavender -fill black -font $2 -pointsize $3 \
-          label:"$4"   -gravity South -append \
-                  -write mpr:image    +delete \
+magick tmp_output.png -resize 550x550\> \
+       -gravity South -append \
+                -write mpr:image    +delete \
           $5             -write mpr:edge_top +delete \
           $5 -rotate 180 -write mpr:edge_btm +delete \
           \
