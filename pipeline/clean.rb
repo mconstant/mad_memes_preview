dc9d043157956fdc7e045b58d9807a42a71162a1
mconstant <85532172+mconstant@users.noreply.github.com>
2023-09-19 18:09:16 +0000

    new clean

--- pipeline/clean.rb
+++ pipeline/clean.rb
@@ -6,6 +6,7 @@ Bundler.require
 include HTTParty
 
 cids = JSON.parse(`curl -G -d "limit=1000" -H "Authorization: Bearer #{ENV['NFT_STORAGE_KEY']}" https://api.nft.storage/`)["value"].map {|entry| entry["cid"]}
+puts "#{cids.count} cids found"
 cids.each do |cid|
     puts `curl -X DELETE -H "Authorization: Bearer #{ENV['NFT_STORAGE_KEY']}" https://api.nft.storage/#{cid}`
     sleep 1
